c8d32576644f53d4a304e3d1878a8b7a6f1fe030
abdulrdeveloper <abdulrahman2040b@gmail.com>
2026-02-13 10:11:30 +0500

    Completed 06-kiryana-store.js problem

--- src/06-kiryana-store.js
+++ src/06-kiryana-store.js
@@ -51,21 +51,58 @@
  *   formatBill([{name:"Atta",price:40,qty:2}]) // => "Atta x 2 = Rs.80"
  */
 export function getItemNames(items) {
-  // Your code here
+  if(!Array.isArray(items)){
+    return [];
+  }
+  function onlynames(item){
+    return item.name;  // iska matlab ke jo item aiy gi ose dekhay agar vo ame word ki he to only ose return kray
+  }
+  return items.map(onlynames);
 }
 
 export function getAffordableItems(items, maxPrice) {
-  // Your code here
+  if(!Array.isArray(items) || typeof maxPrice !== "number"){
+    return [];
+  }
+  function checkprice(item){
+    return item.price <= maxPrice;
+  }
+  return items.filter(checkprice);
 }
 
 export function calculateTotal(items) {
-  // Your code here
+   if(!Array.isArray(items) || items.length === 0 ){
+    return 0;
+  }
+  function sum(total,item){                 // ek total show krwaiy and ek item ko one by one check and process kray ga
+    return total + (item.price*item.qty);     // final total sab calculate kr kay return krday ga and item.price * item.qty input values ko calculate kray ga
+  }
+  return items.reduce(sum,0);      //parameter call kia and 2nd 0 day dia takay total 0 say start ho and add hota jaiy
 }
 
 export function sortByPrice(items, ascending) {
-  // Your code here
+ if(!Array.isArray(items))
+  return [];
+
+function getprice(a,b){
+  if(ascending === true){   // check kia agar ascending he to 
+    return a.price - b.price;     // jo a wala array ka container as a input aiy ga usmay prie dekho and b.price mai bi price dekho and dono ko minus (-) kro jo value aiy gi vo signal hoga ke iss price walay ko pehle rakho uske baad next wali ko rakho ek array mai iss trha se ek tarteeb mai sab aiy ga  | Notes yay result nai deta final yay just a and b values ko dekhta he and jo choti value hoti he ose array mai pehle lga deta he chahay vo a ki ho ya b ki
+  }
+  else{
+    return b.price - a.price;    // agar condition false hoi to yay chlay ga and ise olta llikha he takay ascending true a-b use ho and yay uska olta b-a use ho
+  }
+}
+  return [...items].sort(getprice);
 }
 
+
 export function formatBill(items) {
-  // Your code here
+ if(!Array.isArray(items) || items.length === 0){
+  return "";
+ }
+ function createbill(item){
+  const total = item.price * item.qty;
+  return (`${item.name} x ${item.qty} = Rs.${total}`);
+ }
+ return items.map(createbill).join("\n");
 }
