a2a30adb8f3f3efcfc9b99e7cc35b2a648d5ecec
abdulrdeveloper <abdulrahman2040b@gmail.com>
2026-02-13 12:27:30 +0500

    Completed 09-postcard-writer.js Problem

--- src/09-postcard-writer.js
+++ src/09-postcard-writer.js
@@ -52,21 +52,69 @@
  *   countVowels("Namaste")     // => 3
  */
 export function writePostcard(sender, receiver, message) {
-  // Your code here
+  if(typeof sender !== "string" || typeof receiver !== "string" || typeof message !== "string"){
+    return "";
+  }
+
+   let finalsender = sender.trim();
+   let finalreceiver = receiver.trim();
+   let finalmessage = message.trim();
+   if(finalmessage == "" || finalreceiver == "" || finalsender == ""){
+    return "";
+   }
+  return (`Priy ${finalreceiver},\n\n${finalmessage}\n\nAapka/Aapki,\n${finalsender}`)
 }
 
 export function isValidPincode(code) {
-  // Your code here
+
+  if(typeof code !== "string"){
+    return false;
+  }
+  if(code.startsWith("0")){
+    return false;
+  }
+  if(code.length !== 6){
+    return false;
+  }
+  if(/^\d+$/.test(code)){
+    return true;
+  }
+  else{
+  return false;
+}
 }
 
 export function formatPostcardField(label, value, width) {
-  // Your code here
+
+  if(typeof label !== "string" || typeof value !== "string"){
+    return "";
+  }
+  if(typeof width !== "number"){
+    width = 12;
+  }
+  return label.padEnd(width) + ": " + value;
 }
 
 export function isFromState(address, stateCode) {
-  // Your code here
+  if(typeof address !== "string" || typeof stateCode !== "string"){
+    return false;
+  }
+ if (address.endsWith(stateCode)){
+  return true;
+}
+else{
+  return false;
+}
 }
 
 export function countVowels(message) {
-  // Your code here
+  if(typeof message !== "string"){
+    return 0;
+  }
+   let final = message.match(/[aeiouAEIOU]/g);
+
+   if(final === null){
+    return 0;
+   }
+   return final.length;
 }
